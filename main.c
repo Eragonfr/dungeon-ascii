b0c6f6a5c48d0f217a2d50d47f3da0ff598a91c6
Stévan Corre <stevancorre@gmail.com>
2020-12-03 16:47:58 +0100

    Crossplaform input (maybe)

--- main.c
+++ main.c
@@ -5,6 +5,33 @@
 #define SCREEN_WIDTH 80
 #define SCREEN_HEIGHT 20
 
+#ifdef _WIN32
+
+#include <conio.h>
+
+char readInput()
+{
+    return _getch();
+}
+#endif
+
+#ifdef __unix__
+
+#include "curses.h"
+
+char readInput()
+{
+    initscr();
+    char ch = getch();
+    endwin();
+
+    return ch;
+}
+
+#endif
+
+char readInput();
+
 int main()
 {
     RgbChar map[] = {
@@ -12,6 +39,8 @@ int main()
         { 255, 255, 255, 219 },
         { 255,   0,   0, 219 }
     };
+
+    printf("%c", readInput());
 
     renderMap(map, sizeof(map) / sizeof(map[0]));
 
